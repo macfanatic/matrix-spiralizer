95c7365f79a7721b11a61343949853af4e364273
Matt Millsaps-Brewer <matt@madebylotus.com>
2017-05-03 10:47:28 -0400

    Can now move down from end of top right row

--- app/services/matrices/spiralize.rb
+++ app/services/matrices/spiralize.rb
@@ -17,7 +17,7 @@ module Matrices
       # An exception is raised when we're finished (out of bounds or already attempted that cell)
       [].tap do |results|
         begin
-          results.concat right(IndexPath[0,0], IndexPath[0, matrix.columns])
+          results.concat right!(IndexPath[0,0], IndexPath[0, max_columns])
         rescue AlreadyExtracted, Matrix::IndexPathOutOfBounds
         end
       end
@@ -37,25 +37,60 @@ module Matrices
       @recorded_indices.include?(index)
     end
 
-    def right(origin, stop)
+    def max_rows
+      @max_rows ||= @matrix.rows - 1
+    end
+
+    def max_columns
+      @max_columns ||= @matrix.columns - 1
+    end
+
+    # Moves from origin until the indicated stop point
+    #
+    # Will begin moving down from the end of this row
+    def right!(origin, stop)
       y = origin.column
 
       [].tap do |results|
-        while y < stop.column
+        while y <= stop.column
           results << extract!(IndexPath[origin.row, y])
 
           y += 1
         end
+
+        # Begin moving down from the end of this row
+        starts_at = IndexPath[origin.row + 1, stop.column]
+        ends_at = IndexPath[max_rows - origin.row, stop.column]
+
+        results.concat down!(starts_at, ends_at)
       end
     end
 
-    def left(origin, stop)
+    # Moves from origin until the indicated stop point
+    #
+    # Will begin moving up from beginning of this row
+    def left!(origin, stop)
     end
 
-    def up(origin, stop)
+    # Moves from origin until the indicated stop point
+    #
+    # Will begin moving right from top of this column
+    def up!(origin, stop)
     end
 
-    def down(origin, stop)
+    # Moves from origin until the indicated stop point
+    #
+    # Will begin moving left from bottom of this column
+    def down!(origin, stop)
+      x = origin.row
+
+      [].tap do |results|
+        while x <= stop.row
+          results << @matrix[IndexPath[x, origin.column]]
+
+          x += 1
+        end
+      end
     end
   end
 end
