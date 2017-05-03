8bc3bdb06413fad5c756cb3d26c8fc7f57e64e85
Matt Millsaps-Brewer <matt@madebylotus.com>
2017-05-03 08:50:20 -0400

    Create a boot entry point that loads all code from "app" folder

---
+++ config/boot.rb
@@ -0,0 +1,8 @@
+require 'pathname'
+
+# Add all subfolders of the "app" dir as paths in the load path
+app_dir = File.expand_path('../../app', __FILE__)
+app_directories = Pathname.new(app_dir).children.select(&:directory?)
+app_directories.each do |dir|
+  $LOAD_PATH.unshift(dir) unless $LOAD_PATH.include?(dir)
+end
